48d17ffa1102983ee2f5f6e3f61eb06d892e12ef
Anh Vu Nguyen <anhvu.nguyenlam@gmail.com>
2015-07-19 06:52:32 -0400

    test

--- cultural/cultural.js
+++ cultural/cultural.js
@@ -26,13 +26,13 @@ $(document).ready( function() {
 		
 		var params = {
 				"api-key": "d60f06ee6b233be3b6362cf824aa2c1c:1:61091877",
-				//ll:mydata[1].geometry.coordinates[1]+"," + mydata[1].geometry.coordinates[0],
-				//radius: 5000
+				ll:mydata[1].geometry.coordinates[1]+"," + mydata[1].geometry.coordinates[0],
+				radius: 5000
 		}
 		console.log(params);
 		
 		/*
-		$.getJSON('https://jsonp.afeld.me/?callback=myCallBack&url=http%3A%2F%2Fapi.nytimes.com%2Fsvc%2Fevents%2Fv2%2Flistings%3Fapi-key%3Dd60f06ee6b233be3b6362cf824aa2c1c%253A1%253A61091877%26ll%3D40.6561715%252C-73.9515718%26radius%3D5000', null, function (results) {
+		$.get('https://jsonp.afeld.me/?callback=myCallBack&url=http%3A%2F%2Fapi.nytimes.com%2Fsvc%2Fevents%2Fv2%2Flistings%3Fapi-key%3Dd60f06ee6b233be3b6362cf824aa2c1c%253A1%253A61091877%26ll%3D40.6561715%252C-73.9515718%26radius%3D5000', null, function (results) {
 	        alert('Cross domain JS call achieved. Have your implementation going in here!');
 	      });
 		*/
