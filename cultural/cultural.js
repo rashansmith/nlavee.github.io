327defba9a94449512b56c4d243ee13e8b706b8f
Anh Vu Nguyen <anhvu.nguyenlam@gmail.com>
2015-07-19 06:43:02 -0400

    test

--- cultural/cultural.js
+++ cultural/cultural.js
@@ -31,12 +31,12 @@ $(document).ready( function() {
 		}
 		console.log(params);
 		
-		/*
-		$.getJSON('https://jsonp.afeld.me/?url=http://api.nytimes.com/svc/events/v2/listings?api-key=d60f06ee6b233be3b6362cf824aa2c1c%3A1%3A61091877&ll=40.6561715%2C-73.9515718&radius=5000', null, function (results) {
+		
+		$.getJSON('https://jsonp.afeld.me/?callback=myCallBack&url=http%3A%2F%2Fapi.nytimes.com%2Fsvc%2Fevents%2Fv2%2Flistings%3Fapi-key%3Dd60f06ee6b233be3b6362cf824aa2c1c%253A1%253A61091877%26ll%3D40.6561715%252C-73.9515718%26radius%3D5000', null, function (results) {
 	        alert('Cross domain JS call achieved. Have your implementation going in here!');
 	      });
-		*/
 		
+		/*
 		$.ajax({
 			type:'GET',
 			url:"https://jsonp.afeld.me/?url=http://api.nytimes.com/svc/events/v2/listings",
@@ -46,6 +46,6 @@ $(document).ready( function() {
 			console.log(e);
 			
 		})
-		
+		*/
 	});
 });
