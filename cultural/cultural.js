cdba9f451798f5bfdfdc50021d63c8eee15ce01d
Anh Vu Nguyen <anhvu.nguyenlam@gmail.com>
2015-07-19 07:27:37 -0400

    updating

--- cultural/cultural.js
+++ cultural/cultural.js
@@ -17,6 +17,24 @@ function load_json() {
 	});
 }
 
+
+function initialize(lat, lon) {
+	console.log("getting map..");
+
+	var myLatlng = new google.maps.LatLng(lat, lon);
+	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+		zoom: 15,
+		center: myLatlng
+	});
+
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: event
+	});
+	console.log("map should be appearing..")
+}
+
 $(document).ready( function() {
 	
 	load_json();
@@ -24,29 +42,45 @@ $(document).ready( function() {
 	$("#search").click(function() {
 		console.log("seaching..");
 		
-		var params = {
-				"api-key": "d60f06ee6b233be3b6362cf824aa2c1c:1:61091877",
-				ll:mydata[1].geometry.coordinates[1]+"," + mydata[1].geometry.coordinates[0],
-				radius: 5000
-		}
-		console.log(params);
+		var i = 1;
 		
-		
-		$.get('https://jsonp.afeld.me/?url=http%3A%2F%2Fapi.nytimes.com%2Fsvc%2Fevents%2Fv2%2Flistings%3Fapi-key%3Dd60f06ee6b233be3b6362cf824aa2c1c%253A1%253A61091877%26ll%3D'+mydata[1].geometry.coordinates[1] +'%252C'+mydata[1].geometry.coordinates[0]+'%26radius%3D5000', function (results) {
+		$.get('https://jsonp.afeld.me/?url=http%3A%2F%2Fapi.nytimes.com%2Fsvc%2Fevents%2Fv2%2Flistings%3Fapi-key%3Dd60f06ee6b233be3b6362cf824aa2c1c%253A1%253A61091877%26ll%3D'+mydata[i].geometry.coordinates[1] +'%252C'+mydata[i].geometry.coordinates[0]+'%26radius%3D5000', function (results) {
 	        console.log(results);
+	        var num_results = results['num_results'];
+	        var array = results['results'];
+	        var arrayLength = array.length;
+	        for (var i = 0; i < arrayLength; i++) {
+	            var oneInstance = array[i];
+	            var city = oneInstance['city'];
+	            var free = oneInstance['free'];
+	            var free_event;
+	            var price; 
+	            if(free == false) price = oneInstance['price'];
+	            if(free == true) free_event = "Yes!";
+	            else free_event = "No! :("
+	            
+	            var event_name = oneInstance['event_name'];
+	            $(".results").append(
+	            	'<div class="information" id="information-'+i+'">'
+	            	+'<p>City:' + city + '</p>'
+	            	+'<p>Event:' + event_name + '</p>'
+	            	+'<p>Free:' + free_event + '</p>'
+	            	+'<div id="map-canvas" style="width: 400px; height: 200px"></div>'
+	            	+'</div>'
+	            );
+	            
+	            if(free == false) {
+	            	$("#information-"+i).append(
+	    	            	'<p>Price:' + price + '</p>
+	    	            );
+	            }
+	            var lat = oneInstance['geocode_latitude'];
+	            var lon = oneInstance['geocode_longitude'];
+	            initialize(lat, lon);
+	            
+	        }
 	      });
 		
-		/*
-		
-		$.ajax({
-			type:'GET',
-			url:"http://api.nytimes.com/svc/events/v2/listings",
-			data: params
-		}).done(function (e) {
-			console.log("getting events");
-			console.log(e);
-			
-		})
-		*/
+
 	});
 });
